30efa8e56af4ed8890df4b1af7f94be22f543c18
drophang <victor1998kombad@gmail.com>
2024-03-01 08:31:28 -0400

    Hecho

--- src/Ejemplo3_14.java
+++ src/Ejemplo3_14.java
@@ -1,12 +1,24 @@
-import java.util.Scanner;
-
 public class Ejemplo3_14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int TT = sc.nextInt();
-        double LA = sc.nextDouble();
-        double
-        if (NC)
+        int NZ = sc.nextInt();
+        int PE = sc.nextInt();
+        double CO = sc.nextDouble();
+        if(PE < 5000){
+            System.out.println("No se puede dar el servicio");
+        }else{
+            switch (NZ){
+                case 1 : CO = PE * 11;
+                break;
+                case 2 : CO = PE * 10;
+                break;
+                case 3 : CO = PE * 12;
+                break;
+                case 4 : CO = PE * 24;
+                break;
+                default: CO = PE * 27;
+            }
+            System.out.println("EL costo es " + CO);
+        }
     }
 }
-
