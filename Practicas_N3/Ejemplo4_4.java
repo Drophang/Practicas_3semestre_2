dd2c43e9f04f1eae0cf7266d2c66d436b3ff2ed4
drophang <victor1998kombad@gmail.com>
2024-03-01 09:21:02 -0400

    done

---
+++ Practicas_N3/Ejemplo4_4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejemplo4_4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int SU = 0;
+        int NU = sc.nextInt();
+        int ED = 0;
+        for(int C = 0; C <= NU ; NU++){
+            ED = sc.nextInt();
+            SU = SU + ED;
+        }
+        double PR = SU / NU;
+        System.out.println("El promedio es " + PR);
+    }
+}
