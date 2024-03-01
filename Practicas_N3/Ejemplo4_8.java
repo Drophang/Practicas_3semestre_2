06afa9f6fbacb06ba67e4cfe429f2df4b9b4b263
drophang <victor1998kombad@gmail.com>
2024-03-01 11:18:36 -0400

    4_8

--- Practicas_N3/Ejemplo4_8.java
+++ Practicas_N3/Ejemplo4_8.java
@@ -1,3 +1,12 @@
-public class Ejemplo4_8 {
+import java.util.Scanner;
 
+public class Ejemplo4_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = 0;
+        while (N <= 100) {
+            N = sc.nextInt();
+            N = N + 2;
+        }
+    }
 }
