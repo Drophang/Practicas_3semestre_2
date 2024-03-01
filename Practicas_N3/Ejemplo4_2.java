2bba1d667fbcd7985fdbf823fe518f77d8ece8de
drophang <victor1998kombad@gmail.com>
2024-03-01 09:02:01 -0400

    4_2 Hecho

--- Practicas_N3/Ejemplo4_2.java
+++ Practicas_N3/Ejemplo4_2.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Ejemplo4_2{
-public static void main(String[] args) {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     int SU = 0;
     int C = 1;
     int VA = 0;
     while(C < 10){
+        VA = sc.nextInt();
         SU = SU + VA;
         C = C + 1;
     }
