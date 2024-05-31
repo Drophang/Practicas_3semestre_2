3ba2b23111898e6fcec9b66296db8ab3fc703081
LabF103_PC25 <victor1998kombad@gmail.com>
2024-05-31 08:37:29 -0400

    Echo

--- Arboles/src/Nodo.java
+++ Arboles/src/Nodo.java
@@ -1,4 +1,4 @@
-public class Nodo {
+class Nodo {
     int valor;
     Nodo izquierda;
     Nodo derecha;
@@ -8,49 +8,35 @@ public class Nodo {
         derecha = null;
     }
 }
-class arbolBinario{
+class ArbolBinario{
     Nodo raiz;
-    public arbolBinario(){
+    public ArbolBinario(){
         raiz = null;
     }
     public void insertar(int valor){
         raiz = insertarNodo(raiz,valor);
     }
-    private Nodo insertarNodo(Nodo nodo,int valor){
+    public Nodo insertarNodo(Nodo nodo,int valor){
         if (nodo == null){
             nodo = new Nodo(valor);
-            return nodo;
+            return  nodo;
         }
         if (valor < nodo.valor){
             nodo.izquierda = insertarNodo(nodo.izquierda,valor);
-        }else if (valor > nodo.valor){
+        } else if (valor > nodo.valor) {
             nodo.derecha = insertarNodo(nodo.derecha,valor);
         }
         return nodo;
     }
-    public void imprimirEnorden(){
-        imprimirEnorden();
+    public void imprimirEnOrden(){
+        imprimirEnOrden(raiz);
     }
-    public void imprimirEnorden(Nodo nodo){
-        if(nodo != null){
-            imprimirEnorden(nodo.izquierda);
+    public void imprimirEnOrden(Nodo nodo){
+        if (nodo != null){
+            imprimirEnOrden(nodo.izquierda);
             System.out.println(nodo.valor + " ");
-                imprimirEnorden(nodo.derecha);
+            imprimirEnOrden(nodo.derecha);
         }
-    }
-}
-public class main {
-    public static void main (String[] args){
-        arbolBinario arbol = new arbolBinario();
-        arbol.insertar(50);
-        arbol.insertar(30);
-        arbol.insertar(20);
-        arbol.insertar(40);
-        arbol.insertar(70);
-        arbol.insertar(60);
-        arbol.insertar(80);
-        System.out.println("Arbol en orden");
-        arbol.imprimirEnorden();
     }
 }
 
